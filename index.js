81d8576aa86b79555652e77218937017c134422f
Favour Okoh <okohfavour91@gmail.com>
2023-11-16 13:00:15 +0100

    Feat: View all blog posts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import blogPosts from "./views/store/blogPosts.js";
 
 const app = express();
 const port = 4000;
@@ -6,7 +7,7 @@ const port = 4000;
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {posts: blogPosts})
 })
 
 app.listen(port, () => {
